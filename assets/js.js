a96e889833e853a4cee48a8dbc552d28b51a75dd
ZeroBossSq <82669735+ZeroBossSq@users.noreply.github.com>
2022-06-22 16:20:18 +0300

    Third upd

---
+++ assets/js.js
@@ -0,0 +1,12 @@
+function CopyMe(TextToCopy) {
+    var TempText = document.createElement("input");
+    TempText.value = TextToCopy;
+    document.body.appendChild(TempText);
+    TempText.select();
+
+    document.execCommand("copy");
+    document.body.removeChild(TempText);
+
+    var par = document.querySelector(".par");
+    par.innerHTML = "Copied";
+}
