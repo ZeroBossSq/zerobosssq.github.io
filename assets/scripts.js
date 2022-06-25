350d4543d7482ad8fa4153eee66a7a47ba5744f2
ZeroBossSq <82669735+ZeroBossSq@users.noreply.github.com>
2022-06-25 21:30:59 +0300

    Third upd

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,3 +10,24 @@ function CopyMe(TextToCopy) {
     var par = document.querySelector(".par");
     par.innerHTML = "Copied";
 }
+document.querySelector(".themetoggle").addEventListener("click", (event) => {
+    event.preventDefault();
+    if (localStorage.getItem("theme") === "night-theme") {
+        localStorage.removeItem("theme");
+    } else {
+        localStorage.setItem("theme", "night-theme");
+    }
+    addDarkClassToHTML();
+});
+
+function addDarkClassToHTML() {
+    try {
+        if (localStorage.getItem("theme") === "night-theme") {
+            document.querySelector("html").classList.add("night-theme");
+        } else {
+            document.querySelector("html").classList.remove("night-theme");
+        }
+    } catch (err) {}
+}
+
+addDarkClassToHTML();
